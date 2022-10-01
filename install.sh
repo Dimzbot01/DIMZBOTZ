d3a66c48e52a554f94f42bf34e1e61a8ecfc3157
Dimzbot01 <93210411+Dimzbot01@users.noreply.github.com>
2022-10-01 19:37:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "DONE INSTALLING ALL PACKAGE REQUIRE, RUN npm start"
+echo "WELCOME BACK DIMZ >_<, Click npm start TO RUN!"
